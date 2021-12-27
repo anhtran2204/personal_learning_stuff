568423a9966cc0d34f4d7284423f7cade794f89d
anhtran_2204 <anhphantran2002@gmail.com>
2021-12-27 16:40:52 -0600

    new javascript changes

--- Javascript/FunctionsDemo/function_demo.js
+++ Javascript/FunctionsDemo/function_demo.js
@@ -16,15 +16,51 @@ function fibonacci(n) {
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-const readline = require("readline");
+const readline = require('readline');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
-rl.question("Enter the number: ", function (num) {
-  console.log(factorial(num));
-  console.log(fibonacci(num)); 
-  rl.close();
-});
+rl.question("Enter a number: ", function (answer) {
+    switch(num) {
+        case 1:
+            var user_name = window.prompt("What's your name? ");
+            console.log(`Your name is ${user_name}`);
+            break;
+    
+        case 2:
+            rl.question("What's your age? ", function (age) {
+                console.log("Your age is", age);
+            });
+            break;
+    
+        default:
+            console.log("Nothing to show here!");
+            break;
+    }
+    console.log(`Oh, so your name is ${answer}`);
+    console.log("Closing the interface");
+    rl.close();
+  });
+
+var num = window.prompt("Enter a number: ");
+
+num = Number(num);
+
+switch(num) {
+    case 1:
+        var user_name = window.prompt("What's your name? ");
+        console.log("Your name is", user_name);
+        break;
+
+    case 2:
+        var age = window.prompt("What's your age? ");
+        console.log("Your age is", age);
+        break;
+
+    default:
+        console.log("Nothing to show here!");
+        break;
+}
