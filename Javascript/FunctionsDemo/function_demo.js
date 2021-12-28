a1f91840a597648758b9a511bee21df10957b907
anhtran_2204 <anhphantran2002@gmail.com>
2021-12-28 12:44:46 -0600

    new changes 12/28/21

--- Javascript/FunctionsDemo/function_demo.js
+++ Javascript/FunctionsDemo/function_demo.js
@@ -16,33 +16,54 @@ function fibonacci(n) {
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-const readline = require('readline');
+// const readline = require('readline');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
 
-rl.question("Enter a number: ", function(answer) {
-    switch (+answer) {
-        case 1:
-            rl.question("What's your name? ", function(user_name) {
-                console.log(`Your name is ${user_name}`);
-                rl.close();
-            });
-            break;
+// rl.question("Enter a number: ", function(answer) {
+//     switch (+answer) {
+//         case 1:
+//             rl.question("What's your name? ", function(user_name) {
+//                 console.log(`Your name is ${user_name}`);
+//                 rl.close();
+//             });
+//             break;
 
-        case 2:
-            rl.close();
-            rl.question("What's your age? ", function(age)  {
-                console.log(`Your age is ${age}`);
-                rl.close();
-            });
-            break;
+//         case 2:
+//             rl.close();
+//             rl.question("What's your age? ", function(age)  {
+//                 console.log(`Your age is ${age}`);
+//                 rl.close();
+//             });
+//             break;
 
-        default:
-            console.log("Nothing to show here!");
-            rl.close();
-            break;
-    }
-});
+//         default:
+//             console.log("Nothing to show here!");
+//             rl.close();
+//             break;
+//     }
+// });
+
+
+var num = window.prompt("Enter a number: ");
+
+num = Number(num);
+
+switch(num) {
+    case 1:
+        var user_name = window.prompt("What's your name? ");
+        console.log("Your name is", user_name);
+        break;
+
+    case 2:
+        var age = window.prompt("What's your age? ");
+        console.log("Your age is", age);
+        break;
+
+    default:
+        console.log("Nothing to show here!");
+        break;
+}
