a458682c06ecc163597b378746db92775a983435
anhtran_2204 <anhphantran2002@gmail.com>
2021-12-27 20:51:15 -0600

    new changes
    fixed code in function_demo.js and added a demo.py for python programs

--- Javascript/FunctionsDemo/function_demo.js
+++ Javascript/FunctionsDemo/function_demo.js
@@ -23,44 +23,26 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
-rl.question("Enter a number: ", function (answer) {
-    switch(num) {
+rl.question("Enter a number: ", function(answer) {
+    switch (+answer) {
         case 1:
-            var user_name = window.prompt("What's your name? ");
-            console.log(`Your name is ${user_name}`);
-            break;
-    
-        case 2:
-            rl.question("What's your age? ", function (age) {
-                console.log("Your age is", age);
+            rl.question("What's your name? ", function(user_name) {
+                console.log(`Your name is ${user_name}`);
+                rl.close();
             });
             break;
-    
+
+        case 2:
+            rl.close();
+            rl.question("What's your age? ", function(age)  {
+                console.log(`Your age is ${age}`);
+                rl.close();
+            });
+            break;
+
         default:
             console.log("Nothing to show here!");
+            rl.close();
             break;
     }
-    console.log(`Oh, so your name is ${answer}`);
-    console.log("Closing the interface");
-    rl.close();
-  });
-
-var num = window.prompt("Enter a number: ");
-
-num = Number(num);
-
-switch(num) {
-    case 1:
-        var user_name = window.prompt("What's your name? ");
-        console.log("Your name is", user_name);
-        break;
-
-    case 2:
-        var age = window.prompt("What's your age? ");
-        console.log("Your age is", age);
-        break;
-
-    default:
-        console.log("Nothing to show here!");
-        break;
-}
+});
