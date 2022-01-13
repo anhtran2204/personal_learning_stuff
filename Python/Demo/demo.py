f27d551622d0023c5ff0501212ce50db3e9fe203
anhtran_2204 <anhphantran2002@gmail.com>
2022-01-13 16:21:22 -0600

    New commit for Python and Web Development

--- Python/Demo/demo.py
+++ Python/Demo/demo.py
@@ -30,6 +30,9 @@ prices = [
   2
 ]
 
+combined_list = toppings + prices
+print("Combined List:", combined_list)
+
 num_two_dollar_slices = prices.count(2)
 print(num_two_dollar_slices)
 
@@ -60,3 +63,52 @@ name, age, occupation = my_info
 print(name, age, occupation)
 
 # Loops
+single_digits = range(10)
+squares = []
+
+for digit in single_digits:
+  squares.append(digit ** 2)
+  print(digit)
+
+count = 0
+length = len(single_digits)
+while count < length:
+  print("Hello")
+  count += 1
+
+print(squares)
+
+cubes = [digit ** 3 for digit in single_digits]
+print(cubes)
+
+# Loops cont.
+
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+total_price = 0
+
+for price in prices: 
+  total_price += price
+
+average_price = total_price / len(prices)
+print("Average Haircut Price:", average_price)
+
+new_prices = [price - 5 for price in prices]
+print(new_prices)
+
+total_revenue = 0
+
+for i in range(len(hairstyles)):
+  total_revenue += prices[i] * last_week[i]
+
+print("Total Revenue:", total_revenue)
+
+average_daily_revenue = total_revenue / 7
+
+cuts_under_30 = [hairstyles[i] for i in range(len(new_prices) - 1) if new_prices[i] < 30]
+print(cuts_under_30)
+
