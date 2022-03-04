ff879b804a990cfc1d08dc69882ec6642c6823f8
anhtran_2204 <anhphantran2002@gmail.com>
2022-03-03 21:22:46 -0600

    modified cs1337 hw3

--- C++/UTD-CS-Projects/UTD-SearchingSorting/HW3-CS1337-Anh-Tran.cpp
+++ C++/UTD-CS-Projects/UTD-SearchingSorting/HW3-CS1337-Anh-Tran.cpp
@@ -3,12 +3,12 @@
 //
 
 /*
-	Name: Anh Tran
-	Program name: WordSearch
-	Date Created: 02/09//22
-	Notes:
+    Name: Anh Tran
+    Program name: WordSearch
+    Date Created: 02/09//22
+    Notes:
 
-	Changelog:
+    Changelog:
 
 */
 
@@ -22,69 +22,98 @@ using namespace std;
 
 vector<int> randomNums;
 vector<int> sortedNums;
+vector<int> demo; // Demo vector for testing
 
-void randomGenerator(vector<int>& nums, int size, int limit) {
-	srand(time(0));
+int totalNums;
+int valueLimit;
 
-	for (int i = 0; i < size; i++) {
-		nums.push_back(rand() % limit + 1);
-	}
+bool debugging = false;
+
+void showFlow(string location)
+{
+    if (debugging)
+    {
+        cout << "---- " << location << " ----" << endl;
+        system("pause");
+    }
+} // showFlow
+
+void randomGenerator(vector<int> &nums, int size, int limit)
+{
+    srand(time(0));
+
+    for (int i = 0; i < size; i++)
+    {
+        nums.push_back(rand() % limit + 1);
+    }
 }
 
-void input() {
-    int totalNums;
-    int valueLimit;
+void input()
+{
+    cout << "Welcome to the random number generator!" << endl
+         << "How many numbers would you like to generate?" << endl;
+    cin >> totalNums;
 
-	cout << "Welcome to the random number generator!" << endl
-		 << "How many numbers would you like to generate?" << endl;
-	cin >> totalNums;
+    cout << "What number range should the numbers be in? (1 - 20)" << endl;
+    cin >> valueLimit;
 
-	cout << "What number range should the numbers be in? (1 - 20)" << endl;
-	cin >> valueLimit;
-
-
-	randomGenerator(randomNums, totalNums, valueLimit);
+    randomGenerator(randomNums, totalNums, valueLimit);
 }
 
-void showVectors(vector<int> nums, string vectorName) {
-	cout << endl << vectorName << ":\n"
-		 << "Size: " << nums.size() << endl
-		 << "Capacity: " << nums.capacity() << endl
-		 << "************************************************************" << endl;
-	for (int i = 0; i < nums.size(); i++) {
-		cout << nums[i] << " ";
-	}
-	cout << endl << "************************************************************" << endl;
+void showVectors(vector<int> nums, string vectorName)
+{
+    cout << endl
+         << vectorName << ":\n"
+         << "Size: " << nums.size() << endl
+         << "Capacity: " << nums.capacity() << endl
+         << "************************************************************" << endl;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        cout << nums[i] << " ";
+    }
+    cout << endl
+         << "************************************************************" << endl;
 }
 
-void sortVector(vector<int> nums) {
-	sort(nums.begin(),nums.end()); 
+void sortVector(vector<int> nums)
+{
+    sort(nums.begin(), nums.end());
 }
 
-void bubbleSort(vector<int> nums) {
-    for (int i = 0; i < nums.size(); i++) {
-        for (int j = 0; j < nums.size() - 1; j++) {
-            if (nums[j] > nums[j + 1]) {
+void bubbleSort(vector<int> nums)
+{
+    for (int i = 0; i < nums.size(); i++)
+    {
+        for (int j = 0; j < nums.size() - 1; j++)
+        {
+            if (nums[j] > nums[j + 1])
+            {
                 swap(nums[j], nums[j + 1]);
             }
         }
     }
 }
 
-int binarySearch(vector<int> nums, int searchNum) {
+int binarySearch(vector<int> nums, int searchNum)
+{
     int first = 0;
     int last = nums.size() - 1;
     int middle = (first + last) / 2;
 
-    while (first < last) {
-        if (searchNum == nums[middle]) {
+    while (first < last)
+    {
+        if (searchNum == nums[middle])
+        {
             return middle;
         }
 
-        if (searchNum < nums[middle]) {
+        if (searchNum < nums[middle])
+        {
             last = middle;
             middle = (first + last) / 2;
-        } else {
+        }
+        else
+        {
             first = middle + 1;
             middle = (first + last) / 2;
         }
@@ -92,12 +121,41 @@ int binarySearch(vector<int> nums, int searchNum) {
     return -1;
 }
 
-void deleteDuplicates() {
+int countDuplicates(vector<int> nums, int searchNum)
+{
+    int first = 0;
+    int last = totalNums - 1;
+    int middle = (first + last) / 2;
 
+    int count = 0;
+    while (first < last) {
+        if (searchNum == nums[middle]) {
+            count++;
+        }
+
+        if (searchNum < nums[middle]) {
+            last = middle;
+            middle = (first + last) / 2;
+        }
+
+        if (searchNum > nums[middle]) {
+            first = middle + 1;
+            middle = (first + last) / 2;
+        }
+    }
+    return count;
 }
 
-int main() {
+void deleteDuplicates(vector<int> &nums)
+{
+}
+
+int main()
+{
     input();
-    showVectors(randomNums, "Unsorted Vector");
-    cout << "Search num: " << binarySearch(randomNums, 3);
+    showVectors(demo, "Demo Vector");
+    // cout << "Search num: " << binarySearch(randomNums, 3);
+
+    // cout << "Testing countDuplicates()" << endl;
+    // cout << countDuplicates(demo, 1, 0, totalNums);
 }
---
+++ C++/UTD-CS-Projects/UTD-SearchingSorting/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+cout << "Testing countDuplicates()" << endl;
+    // cout << countDuplicates(demo, 1, 0, totalNums);
