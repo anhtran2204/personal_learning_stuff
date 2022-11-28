296d691d1aa27461c3303d1d7419c3ca4b063b1c
anhtran_2204 <anhphantran2002@gmail.com>
2022-11-28 00:40:38 -0600

    new project Editor Operations

---
+++ UTD-CS-Projects/UTD-2nd-Year/EditorEditOperations/src/Editor.java
@@ -0,0 +1,39 @@
+package EditorEditOperations.src;
+
+import java.io.File;
+
+public class Editor {
+    private File file;
+
+    public Editor(String fileName) {
+        file = new File(fileName);
+    }
+
+    public void saveAs(String newName) {
+
+    }
+
+    public void insert() {
+
+    }
+
+    public int delete() {
+
+    }
+
+    public int find() {
+
+    }
+
+    public int getCursor() {
+
+    }
+
+    public void moveCursor() {
+
+    }
+
+    public int replace() {
+
+    }
+}
