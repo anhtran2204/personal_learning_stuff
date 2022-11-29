f4b7f4c4d7af6b5ee17d225308c26e8971eb8ac4
anhtran_2204 <anhphantran2002@gmail.com>
2022-11-29 01:38:14 -0600

    new changes to Editor

--- UTD-CS-Projects/UTD-2nd-Year/EditorEditOperations/src/Editor.java
+++ UTD-CS-Projects/UTD-2nd-Year/EditorEditOperations/src/Editor.java
@@ -1,39 +1,53 @@
 package EditorEditOperations.src;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Editor {
     private File file;
 
     public Editor(String fileName) {
-        file = new File(fileName);
+        file = Paths.get(Paths.get(".").normalize().toAbsolutePath() + "/src/" + fileName).toFile();
+    }
+
+    public File getFile() {
+        return this.file;
     }
 
     public void saveAs(String newName) {
+        Path oldPath = Paths.get(this.file.getAbsolutePath());
 
+        try {
+            Files.move(oldPath, oldPath.resolveSibling(newName));
+        } catch (IOException ignored) {
+
+        }
     }
 
     public void insert() {
 
     }
 
-    public int delete() {
-
-    }
-
-    public int find() {
-
-    }
-
-    public int getCursor() {
-
-    }
-
-    public void moveCursor() {
-
-    }
-
-    public int replace() {
-
-    }
+//    public int delete() {
+//
+//    }
+//
+//    public int find() {
+//
+//    }
+//
+//    public int getCursor() {
+//
+//    }
+//
+//    public void moveCursor() {
+//
+//    }
+//
+//    public int replace() {
+//
+//    }
 }
