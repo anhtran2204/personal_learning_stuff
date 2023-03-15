00724876dcf7b9bdefb8ce37e9c44b065aa62c53
anhtran_2204 <anhphantran2002@gmail.com>
2023-03-15 00:42:44 -0500

    added user input for countDown.asm

--- UTD-CS-Projects/UTD-2nd-Year/Assembly - CS 2340/03_Anh_Tran/countDown.asm
+++ UTD-CS-Projects/UTD-2nd-Year/Assembly - CS 2340/03_Anh_Tran/countDown.asm
@@ -4,4 +4,11 @@ prompt: .asciiz "Give me a number between 1 and 10 (0 to stop): "
 
 	.text 
 main:
+	li $v0, 4				# Print prompt asking for user input
+	la $a0, prompt
+	syscall
+	
+	li $v0, 5				# Asking for user input
+	syscall
+	
 	
