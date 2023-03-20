d7a9fb4c22dc71cea310298de0ec8a0b17f60597
anhtran_2204 <anhphantran2002@gmail.com>
2023-03-20 13:17:39 -0500

    final version of countdown.asm and doublesum.asm

--- UTD-CS-Projects/UTD-2nd-Year/Assembly - CS 2340/03_Anh_Tran/countDown.asm
+++ UTD-CS-Projects/UTD-2nd-Year/Assembly - CS 2340/03_Anh_Tran/countDown.asm
@@ -1,14 +1,83 @@
-	.data
-length: .word 0
-prompt: .asciiz "Give me a number between 1 and 10 (0 to stop): "
+.data
+	prompt: .asciiz "\nGive me a number between 1 and 10 (0 to stop): "
+	iterative: .asciiz "ITERATIVE: "
+	recursive: .asciiz "RECURSIVE: "
+	space: .asciiz " "
+	new_line: .asciiz "\n"
+	number: .word 0						# Variable to store user input		
 
-	.text 
-main:
-	li $v0, 4				# Print prompt asking for user input
-	la $a0, prompt
-	syscall
+.text 
+	main:
+		li $v0, 4					# Print prompt asking for user input
+		la $a0, prompt
+		syscall
 	
-	li $v0, 5				# Asking for user input
-	syscall
+		li $v0, 5					# Asking for user input
+		syscall
+		sw $v0, number
+		
+		li $v0, 4					# Print out the Iterative ver of count down
+		la $a0, iterative
+		syscall
+		jal itr_countdown				# Jump to iterative procedure
+		
+		li $v0, 4
+		la $a0, new_line
+		syscall
+		
+		li $v0, 4					# Print out the Recursive ver of count down
+		la $a0, recursive
+		syscall
+		jal rec_countdown				# Jump to recursive procedure
+		
+		li $v0, 10					# Exit program
+		syscall
+		
+	itr_countdown:						
+		lw $s0, number					# Load user input into $s0
+		loop:
+			# While $s0 not equal -1 continue
+			# Else return result and exit procedure
+			beq $s0, -1, itr_else			
+		
+			li $v0, 1
+			la $a0, 0($s0)				# Load address of $s0 onto $a0 to print out
+			syscall
+		
+			li $v0, 4
+			la $a0, space
+			syscall
+		
+			addi $s0, $s0, -1			# Decrement the $s0 by 1 for next iteration
+			j loop					# Loop again
+		
+		itr_else:
+			jr $ra					# Return result and exit procdure
 	
-	
+	rec_countdown:
+		lw $s1, number					# Load user input into $s1
+		recursion:
+			add $sp, $sp, -8			# Make space on the stack
+			sw $s1, 4($sp)				# Load $s1 on the stack
+			sw $ra, 0($sp)				# Load $ra on the stack
+		
+			# If $s1 not equal then continue
+			# Else, return result and exit current recursion
+			beq $s1, -1, rec_else				
+		
+			li $v0, 1 
+			la $a0, 0($s1)				# Load address of $s1 onto $a0 to print
+			syscall
+		
+			li $v0, 4
+			la $a0, space
+			syscall
+		
+			addi $s1, $s1, -1			# Decrement the $s0 by 1 for next recursion
+			jal recursion
+		
+		rec_else:
+			lw $s1, 4($sp)				# Return $s1 and clear out the stack
+			lw $ra, 0($sp)				# Return $ra and clear out the stack
+			addi $sp, $sp, 8			# Restore space of the stack
+			jr $ra					# Return result and exit procedure
--- UTD-CS-Projects/UTD-2nd-Year/Assembly - CS 2340/03_Anh_Tran/doubleSum.asm
+++ UTD-CS-Projects/UTD-2nd-Year/Assembly - CS 2340/03_Anh_Tran/doubleSum.asm
@@ -1,70 +1,70 @@
-	.data 
-array: .word 404						# An array with a size of 101 (range 0 to 100 inclusive)
-prompt: .asciiz "Enter an integer (0 to 100): "			
-error: .asciiz "Invalid message!\n"
-result1: .asciiz "The double sum of integers from 0 to "
-result2: .asciiz " is "
-length: .word 0							# Variable to indicate size of array
-sum: .word 0							# Variable to hold sum of double integers
+.data 
+	array: .word 404						# An array with a size of 101 (range 0 to 100 inclusive)
+	prompt: .asciiz "Enter an integer (0 to 100): "			
+	error: .asciiz "Invalid message!\n"
+	result1: .asciiz "The double sum of integers from 0 to "
+	result2: .asciiz " is "
+	length: .word 0							# Variable to indicate size of array
+	sum: .word 0							# Variable to hold sum of double integers
 	
-	.text
-main:	
-	li $v0, 4				# Print prompt asking for user input
-	la $a0, prompt
-	syscall
+.text
+	main:	
+		li $v0, 4				# Print prompt asking for user input
+		la $a0, prompt
+		syscall
 			
-	li $v0, 5				# Asking for user input
-	syscall
+		li $v0, 5				# Asking for user input
+		syscall
 	
-	sw $v0, length				# Store input onto the X variable
-	lw $s1, length				# Load the value in X onto $s1 to compare the value with loop index
-	sll $s1, $s1, 2				# Shift bits to the left to multiply array length to bit size (1 = 4 bits)
-	add $s1, $s1, 4				# Increase by to not let the loop exit at the exact number (index <= length)
+		sw $v0, length				# Store input onto the X variable
+		lw $s1, length				# Load the value in X onto $s1 to compare the value with loop index
+		sll $s1, $s1, 2				# Shift bits to the left to multiply array length to bit size (1 = 4 bits)
+		add $s1, $s1, 4				# Increase by to not let the loop exit at the exact number (index <= length)
 	
-	beq $v0, $zero, errorDisplay		# If input value is 0 then branch to error to input again
-	add $t0, $zero, $zero			# Initialize the loop index = 0
-	add $s0, $zero, $zero			# Initialize the counter = 0
-	add $s2, $zero, $zero			# Initialize the sum = 0
-	j storeArray				# Jump to storing numbers into array
+		beq $v0, $zero, errorDisplay		# If input value is 0 then branch to error to input again
+		add $t0, $zero, $zero			# Initialize the loop index = 0
+		add $s0, $zero, $zero			# Initialize the counter = 0
+		add $s2, $zero, $zero			# Initialize the sum = 0
+		j storeArray				# Jump to storing numbers into array
 	
-errorDisplay:		
-	li $v0, 4				# Print the prompt for invalid value
-	la $a0, error
-	syscall
+	errorDisplay:		
+		li $v0, 4				# Print the prompt for invalid value
+		la $a0, error
+		syscall
 		
-	j main					# Go back to input the valid number input
+		j main					# Go back to input the valid number input
 	
-storeArray:
-	beq $t0, $s1, exit			# If current loop index = length then exit loop
-	sll $t1, $s0, 1				# Multiply 2*currValue 
-	sw $t1, array($t0)			# Store 2*currValue into current array index
-	add $s2, $s2, $t1			# Add 2*currValue to the sum
-	add $s0, $s0, 1				# Increment counter by 1
-	addi $t0, $t0, 4			# Increase index by 4 bits (index++ = +4 bits)
+	storeArray:
+		beq $t0, $s1, exit			# If current loop index = length then exit loop
+		sll $t1, $s0, 1				# Multiply 2*currValue 
+		sw $t1, array($t0)			# Store 2*currValue into current array index
+		add $s2, $s2, $t1			# Add 2*currValue to the sum
+		add $s0, $s0, 1				# Increment counter by 1
+		addi $t0, $t0, 4			# Increase index by 4 bits (index++ = +4 bits)
 	
-	j storeArray				# Loop again until branching
+		j storeArray				# Loop again until branching
 	
-exit:
-	sw $s2, sum				# Store the sum in the regiter into the variable
+	exit:
+		sw $s2, sum				# Store the sum in the regiter into the variable
 	
-	li $v0, 4				# Print result 
-	la $a0, result1
-	syscall
+		li $v0, 4				# Print result 
+		la $a0, result1
+		syscall
 	
-	li $v0, 1
-	lw $a0, length
-	syscall
+		li $v0, 1
+		lw $a0, length
+		syscall
+		
+		li $v0, 4
+		la $a0, result2
+		syscall
+		
+		li $v0, 1
+		lw $a0, sum
+		syscall
 	
-	li $v0, 4
-	la $a0, result2
-	syscall
-	
-	li $v0, 1
-	lw $a0, sum
-	syscall
-	
-	li $v0, 10				# Exit program
-	syscall		
+		li $v0, 10				# Exit program
+		syscall		
 				
 					
 							
