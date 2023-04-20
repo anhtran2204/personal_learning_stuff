5b595354f51a098a9d80fb3f0171ffac84bce712
anhtran_2204 <anhphantran2002@gmail.com>
2023-04-20 12:03:04 -0500

    fixed the infinite bug in dots_boxes_demo.asm and added new subroutines and printed out the board for testing index update

--- UTD-CS-Projects/UTD-2nd-Year/Assembly - CS 2340/byte_demo.asm
+++ UTD-CS-Projects/UTD-2nd-Year/Assembly - CS 2340/byte_demo.asm
@@ -1,10 +1,22 @@
 	.data
-byteVar:	.byte '1'
+byteVar1:	.byte '1'
+byteVar2: 	.byte '2'
+newLine: 	.byte '\n'
 
 	.text
 main:
-	la $t0, byteVar
-	lb $a0, 0($t0)
+	
+	li $t0, 0
+	#la $t0, byteVar1
+	lb $a0, byteVar1($t0)
+	li $v0, 11
+	syscall
+	
+	lb $a0, newLine($0)
+	li $v0, 11
+	syscall
+	
+	lb $a0, byteVar2($0)
 	li $v0, 11
 	syscall
 	
--- UTD-CS-Projects/UTD-2nd-Year/Assembly - CS 2340/dots_boxes_demo.asm
+++ UTD-CS-Projects/UTD-2nd-Year/Assembly - CS 2340/dots_boxes_demo.asm
@@ -28,46 +28,104 @@ col2:			.space 4
 
 	.text
 main:
-	jal createBoard
+	#jal createBoard
 	
-	#jal askInput
+	jal askInput
 	
 	j exit
 	
-askInput:
+addSpace:
+	sw $ra, ($sp)
+	lb $a0, space($0)
+	li $v0, 11
+	syscall
+	lw $ra, ($sp)
+	jr $ra
+
+addNewLine:
+	sw $ra, ($sp)
+	lb $a0, newLine($0)
+	li $v0, 11
+	syscall
+	lw $ra, ($sp)
+	jr $ra
 	
+askInput:
+	li $v0, 4
+	la $a0, msg1
+	syscall
+
+	li $v0, 8
+	la $a0, input
+	li $a1, 4
+	syscall
+	
+	jal addNewLine
+	
+	li $t0, 0
+	
+	lb $a0, input($t0)
+	li $v0, 11
+	syscall
+	sb $a0, row1	
+	addi $t0, $t0, 1
+	
+	jal addNewLine
+		
+	lb $a0, input($t0)
+	li $v0, 11
+	syscall
+	addi $a0, $a0, -48
+	sb $a0, col1
+	addi $t0, $t0, 1
+	
+	jal addNewLine
+	
+	lb $a0, input($t0)
+	li $v0, 11
+	syscall
+	sb $a0, row2
+	addi $t0, $t0, 1
+
+	jal addNewLine
+		
+	lb $a0, input($t0)
+	li $v0, 11
+	syscall
+	addi $a0, $a0, -48
+	sb $a0, col2	
 	
 createBoard:
+	sw $ra, ($sp)
 	lw $t0, rowSize
 	lw $t1, colSize
-	#lw $s0, ($a0)
-	#lw $s1, ($a1)
+	
+	jal findIndex
 	
 	li $t2, 0
-	li $t3,0
+	li $t3, 0
 	outer:
-		bge $t2, $t0, endOuter
 		li $t4, 0
 		inner:
-			bge $t4, $t1, endInner
 			lb $a0, board($t3)
 			li $v0, 11
 			syscall
-			addi $t2, $t2, 1
+			addi $t4, $t4, 1
 			addi $t3, $t3, 1
 			
-			j inner
+			ble  $t4, $t1, inner
 		endInner:
 			addi $t2, $t2, 1
+			lb $a0, newLine($0)
 			li $v0, 11
-			lb $a0, newLine
 			syscall
 			
-			j outer
+			ble $t2, $t0, outer
 	endOuter:
+		lb $a0, newLine($0)
 		li $v0, 11
-		lb $a0, newLine
 		syscall
+		lw $ra, ($sp)
 		jr $ra
 
 findIndex:
