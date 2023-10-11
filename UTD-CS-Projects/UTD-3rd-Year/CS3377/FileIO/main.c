28970398f22d21b56bc626ce04fa7904321c9be7
anhtran_2204 <anhphantran2002@gmail.com>
2023-10-11 18:51:03 -0500

    new commit for FileIO

--- UTD-CS-Projects/UTD-3rd-Year/CS3377/FileIO/main.c
+++ UTD-CS-Projects/UTD-3rd-Year/CS3377/FileIO/main.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Type your full name: ");
+    printf("Type your full name and GPA: ");
     char name[100];
+    float gpa;
     fgets(name, sizeof(name), stdin);
-    printf("Hello %s", name);
+    scanf("%f", &gpa);
+
+    FILE *fptr;
+    fptr = fopen("file.txt","w");
+
+    if (fptr == NULL) {
+        printf("Error! File not exist!");
+        exit(1);
+    }
+
+    fprintf(fptr, "%s", name);
+    fprintf(fptr, "%f", gpa);
 
     return 0;
 }
