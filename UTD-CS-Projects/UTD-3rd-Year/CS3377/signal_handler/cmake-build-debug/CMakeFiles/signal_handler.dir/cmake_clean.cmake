c27dad73548e6d0d7a221ed09eca2c5f98eed246
anhtran_2204 <anhphantran2002@gmail.com>
2023-11-20 21:18:45 -0600

    new changes to C project files

---
+++ UTD-CS-Projects/UTD-3rd-Year/CS3377/signal_handler/cmake-build-debug/CMakeFiles/signal_handler.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/signal_handler.dir/error.c.o"
+  "CMakeFiles/signal_handler.dir/error.c.o.d"
+  "CMakeFiles/signal_handler.dir/main.c.o"
+  "CMakeFiles/signal_handler.dir/main.c.o.d"
+  "signal_handler"
+  "signal_handler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/signal_handler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
