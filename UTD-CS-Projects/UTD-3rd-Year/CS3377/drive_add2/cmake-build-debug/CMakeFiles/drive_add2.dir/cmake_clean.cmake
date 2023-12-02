2b815611cf8ac4735822107c71057c9d80bb5b6f
anhtran_2204 <anhphantran2002@gmail.com>
2023-12-02 14:34:54 -0600

    new changes to latest C projects for CS3377 assignments

---
+++ UTD-CS-Projects/UTD-3rd-Year/CS3377/drive_add2/cmake-build-debug/CMakeFiles/drive_add2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/drive_add2.dir/main.c.o"
+  "CMakeFiles/drive_add2.dir/main.c.o.d"
+  "drive_add2"
+  "drive_add2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/drive_add2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ UTD-CS-Projects/UTD-3rd-Year/CS3377/drive_add2/cmake-build-debug/CMakeFiles/drive_add2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for drive_add2.
+# This may be replaced when dependencies are built.
