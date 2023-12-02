c74165c40de08115ac6642adb8a83bb2cb1ec2bc
anhtran_2204 <anhphantran2002@gmail.com>
2023-12-02 02:33:04 -0600

    new changes to latest C projects for CS3377 assignments

---
+++ UTD-CS-Projects/UTD-3rd-Year/CS3377/file_to_pager/cmake-build-debug/CMakeFiles/file_to_pager.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/file_to_pager.dir/error.c.o"
+  "CMakeFiles/file_to_pager.dir/error.c.o.d"
+  "CMakeFiles/file_to_pager.dir/file_to_pager.c.o"
+  "CMakeFiles/file_to_pager.dir/file_to_pager.c.o.d"
+  "file_to_pager"
+  "file_to_pager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/file_to_pager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
