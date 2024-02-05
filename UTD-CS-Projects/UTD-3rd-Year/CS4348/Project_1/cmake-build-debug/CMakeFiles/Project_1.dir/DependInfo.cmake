dee14125bee44cb4c78fbcaa4f02274ecc9392ae
Jason Tran <anhphantran2002@gmail.com>
2024-02-05 00:53:51 -0600

    finished setting up the forking process of CS 4348 Project 1, and the switch statements for the instruction set; finished the implementation of classes and object in Python folder for CS4337

---
+++ UTD-CS-Projects/UTD-3rd-Year/CS4348/Project_1/cmake-build-debug/CMakeFiles/Project_1.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jasontran2048/personal_learning_stuff/UTD-CS-Projects/UTD-3rd-Year/CS4348/Project_1/main.c" "CMakeFiles/Project_1.dir/main.c.o" "gcc" "CMakeFiles/Project_1.dir/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
