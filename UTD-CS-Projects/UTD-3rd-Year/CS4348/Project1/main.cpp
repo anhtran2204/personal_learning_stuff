27e8024e8fbe1d07b6208d8d906b3036c087356d
Jason Tran <anhphantran2002@gmail.com>
2024-02-07 19:27:01 -0600

    changes to file I/O and initial addition to read/write piping for cpu <-> memory processes

--- UTD-CS-Projects/UTD-3rd-Year/CS4348/Project1/main.cpp
+++ UTD-CS-Projects/UTD-3rd-Year/CS4348/Project1/main.cpp
@@ -9,136 +9,172 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    int fd[2];
+int *load_data(const char *file_name);
+
+int main(int argc, const char *argv[]) {
+    int mem_to_cpu[2];
+    int cpu_to_mem[2];
     pid_t pid;
-    int PC, SP, IR, AC, X, Y;
-    int instructions;
 
-    ifstream file;
-    file.open("./sample1.txt");
+    int result1 = pipe(mem_to_cpu);
+    int result2 = pipe(cpu_to_mem);
 
-    if (!file.is_open()) {
-        PERROR("The file %s could not be opened", argv[1]);
-        exit( EXIT_FAILURE );
+    if (result1 < 0 || result2 < 0) {
+        PERROR("pipe() failed");
+        exit(1);
     }
 
-    string line;
-    while (getline(file, line)) {
-        string::size_type n = line.find("//");
-        if (n != string::npos) {
-            line.erase(n);
-        }
+    const char *file = "sample0.txt";
+    load_data(file);
 
-        istringstream iss(line);
-
-        double atype;
-        while (iss >> atype) {
-            cout << atype << endl;
-        }
+    int buf[100];
+    fflush(0);
+    pid = fork();
+    if (pid < 0) {
+        PERROR("fork() failed!");
+        exit(1);
     }
+    else if (pid == 0) {
+        // Memory process
+        int *memory = load_data(file);
 
-    int result = fork();
-    if (result == 0) {
-        // child process
-
+        int count = 0;
+        while (true) {
+            close(mem_to_cpu[0]);
+            buf[count] = memory[count];
+            write(mem_to_cpu[1], &buf[0], sizeof(buf[0]));
+            close(mem_to_cpu[1]);
+            count++;
+        }
     } else {
-        // parent process
-        switch (instructions) {
-            case 1:         // Load value into AC
-                break;
+        // CPU process
+        int PC, SP, IR, AC, X, Y;
+        int instructions;
 
-            case 2:
-                break;
-
-            case 3:
-                break;
-
-            case 4:
-                break;
-
-            case 5:
-                break;
-
-            case 6:
-                break;
-
-            case 7:
-                break;
-
-            case 8:
-                break;
-
-            case 9:
-                break;
-
-            case 10:
-                break;
-
-            case 11:
-                break;
-
-            case 12:
-                break;
-
-            case 13:
-                break;
-
-            case 14:
-                break;
-
-            case 15:
-                break;
-
-            case 16:
-                break;
-
-            case 17:
-                break;
-
-            case 18:
-                break;
-
-            case 19:
-                break;
-
-            case 20:
-                break;
-
-            case 21:
-                break;
-
-            case 22:
-                break;
-
-            case 23:
-                break;
-
-            case 24:
-                break;
-
-            case 25:
-                break;
-
-            case 26:
-                break;
-
-            case 27:
-                break;
-
-            case 28:
-                break;
-
-            case 29:
-                break;
-
-            case 30:
-                break;
-
-            case 50:
-                break;
+        while (true) {
+            close(mem_to_cpu[1]);
+            read(mem_to_cpu[0], &instructions, sizeof(instructions));
+            if (instructions == 50) {
+                exit(0);
+            }
+            cout << instructions << endl;
+            close(mem_to_cpu[0]);
         }
+
+//        switch (instructions) {
+//            case 1:         // Load value into AC
+//                break;
+//
+//            case 2:
+//                break;
+//
+//            case 3:
+//                break;
+//
+//            case 4:
+//                break;
+//
+//            case 5:
+//                break;
+//
+//            case 6:
+//                break;
+//
+//            case 7:
+//                break;
+//
+//            case 8:
+//                break;
+//
+//            case 9:
+//                break;
+//
+//            case 10:
+//                break;
+//
+//            case 11:
+//                break;
+//
+//            case 12:
+//                break;
+//
+//            case 13:
+//                break;
+//
+//            case 14:
+//                break;
+//
+//            case 15:
+//                break;
+//
+//            case 16:
+//                break;
+//
+//            case 17:
+//                break;
+//
+//            case 18:
+//                break;
+//
+//            case 19:
+//                break;
+//
+//            case 20:
+//                break;
+//
+//            case 21:
+//                break;
+//
+//            case 22:
+//                break;
+//
+//            case 23:
+//                break;
+//
+//            case 24:
+//                break;
+//
+//            case 25:
+//                break;
+//
+//            case 26:
+//                break;
+//
+//            case 27:
+//                break;
+//
+//            case 28:
+//                break;
+//
+//            case 29:
+//                break;
+//
+//            case 30:
+//                break;
+//
+//            case 50:
+//                break;
+//        }
     }
     
     return 0;
+}
+
+int *load_data(const char *file_name) {
+    static int memory[2000];
+    char buf[1000];
+
+    FILE *file;
+    file = fopen(file_name, "r");
+
+    if (file == NULL) {
+        PERROR("The file %s could not be opened", file_name);
+        exit( EXIT_FAILURE );
+    }
+
+    while(fgets(buf, sizeof(buf), file) != NULL) {
+
+    }
+    close(file);
+    return memory;
 }
