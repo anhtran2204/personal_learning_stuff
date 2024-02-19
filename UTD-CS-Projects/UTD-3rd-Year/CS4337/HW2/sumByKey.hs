8bd8f9ea9b6f7277e94f45d064c579df5e83840d
Jason Tran <anhphantran2002@gmail.com>
2024-02-19 14:33:17 -0600

    created haskell hw file sumPairs.hs

--- UTD-CS-Projects/UTD-3rd-Year/CS4337/HW2/sumByKey.hs
+++ UTD-CS-Projects/UTD-3rd-Year/CS4337/HW2/sumByKey.hs
@@ -1,13 +1,8 @@
-import Control.Monad.Except
-
-sumByKey :: [(Int, Int)] -> Either String (Int, Int)
+sumByKey :: [(Int, Int)] -> (Int, Int)
 sumByKey pairs = sumByKeyHelper pairs (0, 0)
   where
-    sumByKeyHelper [] acc = Right acc
+    sumByKeyHelper [] acc = acc
     sumByKeyHelper ((k,v):xs) (sum0, sum1)
       | k == 0 = sumByKeyHelper xs (sum0 + v, sum1)
       | k == 1 = sumByKeyHelper xs (sum0, sum1 + v)
-      | otherwise = Left "Invalid key found. Only 0 or 1 are allowed."
-
-main :: IO()
-main = print (sumByKey [(2,1),(1,2),(1,3),(0,4),(0,3)])
+      | otherwise = error "Invalid key found. Only 0 or 1 are allowed."
