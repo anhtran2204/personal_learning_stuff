26d46d9b6794d4f6de9154b6f093f348ca33e1be
Jason Tran <anhphantran2002@gmail.com>
2024-03-04 00:57:09 -0600

    added an addToHistory function that concat the value evaluated from the expression to a list for recursive reference; added a getFromHistory function to retrieve the value for new evaluation - CS4337 Project1

--- UTD-CS-Projects/UTD-3rd-Year/CS4337/Project1/project1.hs
+++ UTD-CS-Projects/UTD-3rd-Year/CS4337/Project1/project1.hs
@@ -1,42 +1,65 @@
 module Main where
 
-    import Data.Char ( isDigit )
+    type History = [Double]
 
     prompt :: IO ()
-    prompt = putStrLn "Please enter the expression: "
+    prompt = putStrLn "Enter expression: "
 
+    
+    addToHistory :: [a] -> a -> [a]
+    addToHistory history value = history ++ [value]
 
-    prefixCalc :: [Char] -> IO ()
-    prefixCalc [] = putStrLn "End of expression"
-    prefixCalc (x:xs)
-        | x == '+' =  do
-            putStrLn "Addition"
-            prefixCalc xs
-        | x == '-' = do
-            putStrLn "Subtraction"
-            prefixCalc xs
-        | x == '*' = do
-            putStrLn "Multiplication"
-            prefixCalc xs
-        | x == '/' = do
-            putStrLn "Division"
-        | otherwise = do
-            putChar x
-            putChar '\n'
-            prefixCalc xs
+    getFromHistory :: Int -> [a] -> Maybe a
+    getFromHistory n history
+        | n > 0 && n <= length history = Just (history !! (n - 1))
+        | otherwise                    = Nothing
 
-    add :: Num a => a -> a -> a
-    add a b = a + b
-
-    mult :: Num a => a -> a -> a
-    mult a b = a * b
-
-    div :: (RealFrac a, Integral b) => a -> a -> b
-    div a b = floor (a / b)
+    parseExpression = 
 
     main :: IO ()
     main = do
         prompt
         expression <- getLine
-        putStrLn ("Expression: " ++ expression)
-        prefixCalc expression
+        putStrLn expression
+
+
+    -- import Data.Maybe (fromJust)
+
+    -- -- Function to check if a character is an operator
+    -- isOperator :: Char -> Bool
+    -- isOperator c = c `elem` "+-/*"
+
+    -- -- Function to perform the actual calculation
+    -- calculate :: Char -> Int -> Int -> Int
+    -- calculate op a b = case op of
+    --     '+' -> a + b
+    --     '*' -> a * b
+    --     '/' -> a `div` b
+    --     _   -> error "Unsupported operator"
+
+    -- -- Unary operation for negation
+    -- negateValue :: Int -> Int
+    -- negateValue = negate
+
+    -- -- Function to evaluate a prefix expression with unary negation
+    -- evalPrefix :: String -> Int
+    -- evalPrefix = head . foldr process []
+    --     where
+    --         process :: Char -> [Int] -> [Int]
+    --         process c stack
+    --             | c == '-' && length stack == 1 = 
+    --                 let a = head stack in negateValue a : tail stack
+    --             | isOperator c = let (a:b:rest) = stack in calculate c a b : rest
+    --             | otherwise = digitToInt c : stack
+
+    --         digitToInt :: Char -> Int
+    --         digitToInt c
+    --             | c `elem` ['0'..'9'] = fromEnum c - fromEnum '0'
+    --             | otherwise = error "Invalid digit"
+
+    -- -- Main function to output the calculation result
+    -- main :: IO ()
+    -- main = do
+    --     putStrLn "Enter expression: "
+    --     preExp <- getLine
+    --     putStrLn $ "Result: " ++ show (evalPrefix preExp)
