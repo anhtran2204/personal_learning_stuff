16661e077498318b25a702fb12cd09687259dcd6
Jason Tran <anhphantran2002@gmail.com>
2024-03-04 20:44:13 -0600

    sample program for CS 4337 Project1; still need more debugging due to no parsing error

--- UTD-CS-Projects/UTD-3rd-Year/CS4337/Project1/project1.hs
+++ UTD-CS-Projects/UTD-3rd-Year/CS4337/Project1/project1.hs
@@ -1,27 +1,97 @@
 module Main where
+    import Data.List
 
-    type History = [Double]
+    history :: [Int]
+    history = []
 
-    prompt :: IO ()
-    prompt = putStrLn "Enter expression: "
+    add_to_history :: Int -> [Int]
+    add_to_history value = history ++ [value]
 
-    
-    addToHistory :: [a] -> a -> [a]
-    addToHistory history value = history ++ [value]
+    get_from_history :: Int -> Int
+    get_from_history n = history !! (n - 1)
 
-    getFromHistory :: Int -> [a] -> Maybe a
-    getFromHistory n history
-        | n > 0 && n <= length history = Just (history !! (n - 1))
-        | otherwise                    = Nothing
+    parse_expression :: [String] -> Either String (Int, [String])
+    parse_expression [] = Left "Empty expression"
+    parse_expression (token:tokens)
+        | token == "+" = do
+            (val1, tokens1) <- parse_expression tokens
+            (val2, tokens2) <- parse_expression tokens1
+            return (val1 + val2, tokens2)
+        | token == "*" = do
+            (val1, tokens1) <- parse_expression tokens
+            (val2, tokens2) <- parse_expression tokens1
+            return (val1 * val2, tokens2)
+        | token == "/" = do
+            (val1, tokens1) <- parse_expression tokens
+            (val2, tokens2) <- parse_expression tokens1
+            if val2 == 0
+            then Left "Division by zero"
+            else return (val1 `div` val2, tokens2)
+        | token == "-" = do
+            (val1, tokens1) <- parse_expression tokens
+            return (-val1, tokens1)
+        | "$" `isPrefixOf` token = do
+            let n = read (drop 1 token) :: Int
+            return (get_from_history n, tokens)
+        | otherwise = do
+            let val = read token :: Int
+            return (val, tokens)
 
-    parseExpression = 
+    eval_expression :: String -> Either String String
+    eval_expression expr = do
+        let tokens = words expr
+        (result, remaining) <- parse_expression tokens
+        if remaining == [] 
+            then Right (show (add_to_history result))
+            else Left "Invalid Expression"
+
+    main_loop :: IO ()
+    main_loop = do
+        putStrLn "Enter expression (or type 'exit' to quit): "
+        expr <- getLine
+        if expr == "exit"
+            then return ()
+            else do
+                case eval_expression expr of
+                    Right result -> do
+                        putStrLn result
+                        main_loop
+                    Left error -> do
+                        putStrLn error
+                        main_loop
 
     main :: IO ()
-    main = do
-        prompt
-        expression <- getLine
-        putStrLn expression
+    main = main_loop
 
+
+    -- import Data.Char 
+    -- import Data.List (isPrefixOf)
+
+    -- type Token = String
+    -- type Tokens = [String]
+    -- type History = [Double]
+
+    -- prompt :: IO ()
+    -- prompt = putStrLn "Enter expression: "
+
+
+    -- addToHistory :: [a] -> a -> [a]
+    -- addToHistory history value = history ++ [value]
+
+    -- getFromHistory :: Int -> [a] -> Maybe a
+    -- getFromHistory n history
+    --     | n > 0 && n <= length history = Just (history !! (n - 1))
+    --     | otherwise                    = Nothing
+
+    -- parseExpression [] = (Left "Empty expression", [])
+    -- parseExpression ()
+
+    -- main :: IO ()
+    -- main = do
+    --     putStrLn "Enter expression (or type 'exit' to quit): "
+    --     expression <- getLine
+    --     if expression == "exit" then return ()
+    --     else putStrLn expression
 
     -- import Data.Maybe (fromJust)
 
