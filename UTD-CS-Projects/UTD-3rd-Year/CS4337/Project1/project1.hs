c7b1e408963e73160b73486783e7bcfadfefed4c
Jason Tran <anhphantran2002@gmail.com>
2024-03-05 16:02:54 -0600

    minor changes to the types of the functions (parse_expression, eval_expression) - CS4337 Project1

--- UTD-CS-Projects/UTD-3rd-Year/CS4337/Project1/project1.hs
+++ UTD-CS-Projects/UTD-3rd-Year/CS4337/Project1/project1.hs
@@ -1,5 +1,8 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
 module Main where
-    import Data.List
+    import Data.List ( isPrefixOf )
+    import Debug.Trace ( trace )
 
     history :: [Int]
     history = []
@@ -10,38 +13,37 @@ module Main where
     get_from_history :: Int -> Int
     get_from_history n = history !! (n - 1)
 
-    parse_expression :: [String] -> Either String (Int, [String])
+    parse_expression :: String -> Either String (Int, String)
     parse_expression [] = Left "Empty expression"
     parse_expression (token:tokens)
-        | token == "+" = do
+        | token == '+' = do
             (val1, tokens1) <- parse_expression tokens
             (val2, tokens2) <- parse_expression tokens1
             return (val1 + val2, tokens2)
-        | token == "*" = do
+        | token == '*' = do
             (val1, tokens1) <- parse_expression tokens
             (val2, tokens2) <- parse_expression tokens1
             return (val1 * val2, tokens2)
-        | token == "/" = do
+        | token == '/' = do
             (val1, tokens1) <- parse_expression tokens
             (val2, tokens2) <- parse_expression tokens1
             if val2 == 0
             then Left "Division by zero"
             else return (val1 `div` val2, tokens2)
-        | token == "-" = do
+        | token == '-' = do
             (val1, tokens1) <- parse_expression tokens
             return (-val1, tokens1)
-        | "$" `isPrefixOf` token = do
-            let n = read (drop 1 token) :: Int
-            return (get_from_history n, tokens)
+        | "$" `isPrefixOf` (token:tokens) = do
+            let n = read (read $ show (head tokens) :: String) :: Int 
+            return (get_from_history n, tokens) 
         | otherwise = do
-            let val = read token :: Int
+            let val = read $ show token :: Int 
             return (val, tokens)
 
     eval_expression :: String -> Either String String
     eval_expression expr = do
-        let tokens = words expr
-        (result, remaining) <- parse_expression tokens
-        if remaining == [] 
+        (result, remaining) <- parse_expression expr
+        if null remaining
             then Right (show (add_to_history result))
             else Left "Invalid Expression"
 
@@ -62,74 +64,3 @@ module Main where
 
     main :: IO ()
     main = main_loop
-
-
-    -- import Data.Char 
-    -- import Data.List (isPrefixOf)
-
-    -- type Token = String
-    -- type Tokens = [String]
-    -- type History = [Double]
-
-    -- prompt :: IO ()
-    -- prompt = putStrLn "Enter expression: "
-
-
-    -- addToHistory :: [a] -> a -> [a]
-    -- addToHistory history value = history ++ [value]
-
-    -- getFromHistory :: Int -> [a] -> Maybe a
-    -- getFromHistory n history
-    --     | n > 0 && n <= length history = Just (history !! (n - 1))
-    --     | otherwise                    = Nothing
-
-    -- parseExpression [] = (Left "Empty expression", [])
-    -- parseExpression ()
-
-    -- main :: IO ()
-    -- main = do
-    --     putStrLn "Enter expression (or type 'exit' to quit): "
-    --     expression <- getLine
-    --     if expression == "exit" then return ()
-    --     else putStrLn expression
-
-    -- import Data.Maybe (fromJust)
-
-    -- -- Function to check if a character is an operator
-    -- isOperator :: Char -> Bool
-    -- isOperator c = c `elem` "+-/*"
-
-    -- -- Function to perform the actual calculation
-    -- calculate :: Char -> Int -> Int -> Int
-    -- calculate op a b = case op of
-    --     '+' -> a + b
-    --     '*' -> a * b
-    --     '/' -> a `div` b
-    --     _   -> error "Unsupported operator"
-
-    -- -- Unary operation for negation
-    -- negateValue :: Int -> Int
-    -- negateValue = negate
-
-    -- -- Function to evaluate a prefix expression with unary negation
-    -- evalPrefix :: String -> Int
-    -- evalPrefix = head . foldr process []
-    --     where
-    --         process :: Char -> [Int] -> [Int]
-    --         process c stack
-    --             | c == '-' && length stack == 1 = 
-    --                 let a = head stack in negateValue a : tail stack
-    --             | isOperator c = let (a:b:rest) = stack in calculate c a b : rest
-    --             | otherwise = digitToInt c : stack
-
-    --         digitToInt :: Char -> Int
-    --         digitToInt c
-    --             | c `elem` ['0'..'9'] = fromEnum c - fromEnum '0'
-    --             | otherwise = error "Invalid digit"
-
-    -- -- Main function to output the calculation result
-    -- main :: IO ()
-    -- main = do
-    --     putStrLn "Enter expression: "
-    --     preExp <- getLine
-    --     putStrLn $ "Result: " ++ show (evalPrefix preExp)
