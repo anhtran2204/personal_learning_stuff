25cff3a9103e43bbebabb0d514ff9f9cfe57bd56
Jason Tran <anhphantran2002@gmail.com>
2024-03-19 16:41:33 -0500

    initialized more constant vars and semaphores for the project; added a simple to illustrate creating a list of threads - CS 4348 Project2

--- UTD-CS-Projects/UTD-3rd-Year/CS4348/Project2/project2.cpp
+++ UTD-CS-Projects/UTD-3rd-Year/CS4348/Project2/project2.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <pthread.h>
+#include <semaphore.h>
 
 using namespace std;
+
+/* Constants */
+const int TIMER = 100; // milliseconds of pause between steps
+const int MAX_PATIENTS = 15; // max number of patient threads
+const int MAX_DOCTORS = 3; // max number of doctor threads
 
 vector<pthread_t> doctors;
 vector<pthread_t> nurses;
 vector<pthread_t> patients;
+
+/* Shared Semaphores */
+sem_t consumeReceptionist;
+sem_t produceReceptionist;
+sem_t waitingRoomSem;
+sem_t enteredSem;
+sem_t doctorSem;
+sem_t nurseSem;
 
 void* run(void* id) {
     printf("thread #%d created!\n", id);
@@ -15,6 +29,11 @@ void* run(void* id) {
 
 int main(int argc, char const *argv[])
 {
+    if (argc != 3) {
+        printf("Usage: %s  <number_of_doctors> <number_of_patients>", argv[0]);
+        return -1;
+    }
+
     for (int i = 0; i < 3; i++)
     {
         pthread_t doctor;
