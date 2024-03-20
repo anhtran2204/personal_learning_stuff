3492c183522b2cc1b8539a05db94e3425a9269ba
Jason Tran <anhphantran2002@gmail.com>
2024-03-19 19:39:50 -0500

    added a test program to test out created threads with a sempahore function

--- UTD-CS-Projects/UTD-3rd-Year/CS4348/Project2/project2.cpp
+++ UTD-CS-Projects/UTD-3rd-Year/CS4348/Project2/project2.cpp
@@ -2,11 +2,12 @@
 #include <vector>
 #include <pthread.h>
 #include <semaphore.h>
+#include <unistd.h>
 
 using namespace std;
 
 /* Constants */
-const int TIMER = 100; // milliseconds of pause between steps
+const int TIMER = 1; // seconds of pause between steps
 const int MAX_PATIENTS = 15; // max number of patient threads
 const int MAX_DOCTORS = 3; // max number of doctor threads
 
@@ -22,25 +23,43 @@ sem_t enteredSem;
 sem_t doctorSem;
 sem_t nurseSem;
 
+/* Test Semaphores */
+sem_t demoSem;
+
+void init_sem() {
+    sem_init(&demoSem, 0, 1);
+}
+
 void* run(void* id) {
+    sem_wait(&demoSem);
     printf("thread #%d created!\n", id);
-    pthread_exit(NULL);
+    sleep(TIMER);
+    // pthread_exit(NULL);
+    sem_post(&demoSem);
 }
 
 int main(int argc, char const *argv[])
 {
-    if (argc != 3) {
-        printf("Usage: %s  <number_of_doctors> <number_of_patients>", argv[0]);
-        return -1;
-    }
+    // if (argc != 3) {
+    //     printf("Usage: %s  <number_of_doctors> <number_of_patients>", argv[0]);
+    //     return -1;
+    // }
+
+    /* section for testing functions */
+    init_sem();
+    int rc;
 
     for (int i = 0; i < 3; i++)
     {
-        pthread_t doctor;
-        int rc = pthread_create(&doctor, NULL, NULL, NULL);
-        doctors.push_back(doctor);
-        printf("thread #%d created.\n", i);
+        pthread_t thread;
+        if (rc = pthread_create(&thread, NULL, run, (void *) i)) {
+            printf("thread creation failed: %d", rc);
+        }
+        pthread_join(thread, NULL);
     }
+    sem_destroy(&demoSem);
+    printf("thread creation complete...\n");
+    /*********************************/
     
     return 0;
 }
