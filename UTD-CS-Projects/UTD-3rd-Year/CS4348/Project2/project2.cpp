db5b6be65cc6ba4d4c47f94a348661f8c32bbc87
Jason Tran <anhphantran2002@gmail.com>
2024-03-20 01:19:17 -0500

    added more testing semaphores but for the doctors, nurses, receptionist, and patients to synchronize the threads from each categories printing out each method

--- UTD-CS-Projects/UTD-3rd-Year/CS4348/Project2/project2.cpp
+++ UTD-CS-Projects/UTD-3rd-Year/CS4348/Project2/project2.cpp
@@ -7,59 +7,152 @@
 using namespace std;
 
 /* Constants */
-const int TIMER = 1; // seconds of pause between steps
+const int TIMER = 1;         // seconds of pause between steps
 const int MAX_PATIENTS = 15; // max number of patient threads
-const int MAX_DOCTORS = 3; // max number of doctor threads
+const int MAX_DOCTORS = 3;   // max number of doctor threads
 
 vector<pthread_t> doctors;
 vector<pthread_t> nurses;
 vector<pthread_t> patients;
 
 /* Shared Semaphores */
-sem_t consumeReceptionist;
-sem_t produceReceptionist;
-sem_t waitingRoomSem;
-sem_t enteredSem;
-sem_t doctorSem;
-sem_t nurseSem;
+// sem_t consumeReceptionist;
+// sem_t produceReceptionist;
+// sem_t waitingRoomSem;
+// sem_t enteredSem;
+// sem_t doctorSem;
+// sem_t nurseSem;
 
 /* Test Semaphores */
 sem_t demoSem;
+sem_t doctorSem;
+sem_t nurseSem;
+sem_t receptionistSem;
+sem_t patientSem;
 
-void init_sem() {
-    sem_init(&demoSem, 0, 1);
+void init_sem()
+{
+    if (sem_init(&demoSem, 0, 1) == -1) {
+        cerr << "Error: semaphore" << endl;
+    }
+    if (sem_init(&doctorSem, 0, 0) == -1) {
+        cerr << "Error: semaphore" << endl;
+    }
+    if (sem_init(&nurseSem, 0, 1)  == -1) {
+        cerr << "Error: semaphore" << endl;
+    }
+    if (sem_init(&receptionistSem, 0, 1) == -1) {
+        cerr << "Error: semaphore" << endl;
+    }
+    if (sem_init(&patientSem, 0, 1) == -1) {
+        cerr << "Error: semaphore" << endl;
+    }
 }
 
-void* run(void* id) {
+void *run(void *id)
+{
     sem_wait(&demoSem);
-    printf("thread #%d created!\n", id);
+    printf("thread #%ld created!\n", (long) id);
     sleep(TIMER);
-    // pthread_exit(NULL);
     sem_post(&demoSem);
+}
+
+void *doctor(void *arg) {
+    sem_wait(&doctorSem);
+    printf("doctor %ld checking...\n", (long) arg);
+    sleep(TIMER);
+    sem_post(&nurseSem);
+}
+
+void *nurse(void *arg) {
+    sem_wait(&nurseSem);
+    printf("nurse %ld attending...\n", (long) arg);
+    sleep(TIMER);
+    sem_post(&doctorSem);
+}
+
+void *receptionist(void *arg) {
+    sem_wait(&receptionistSem);
+    printf("receptionist registering...\n");
+    sleep(TIMER);
+    sem_post(&receptionistSem);
+}
+
+void *patient(void *arg) {
+    sem_wait(&patientSem);
+    printf("patient %ld entering...\n", (long) arg);
+    sleep(TIMER);
+    sem_post(&patientSem);
 }
 
 int main(int argc, char const *argv[])
 {
-    // if (argc != 3) {
-    //     printf("Usage: %s  <number_of_doctors> <number_of_patients>", argv[0]);
-    //     return -1;
-    // }
+    if (argc != 3)
+    {
+        printf("Usage: %s  <number_of_doctors> <number_of_patients>", argv[0]);
+        return -1;
+    }
 
     /* section for testing functions */
     init_sem();
     int rc;
 
-    for (int i = 0; i < 3; i++)
+    pthread_t receptionistThread;
+    if (rc = pthread_create(&receptionistThread, NULL, receptionist, NULL))
     {
-        pthread_t thread;
-        if (rc = pthread_create(&thread, NULL, run, (void *) i)) {
-            printf("thread creation failed: %d", rc);
-        }
-        pthread_join(thread, NULL);
+        printf("Error: pthread_create() failed\n");
+        exit(EXIT_FAILURE);
     }
+    sleep(TIMER);
+    for (long i = 0; i < 3; i++)
+    {
+        // pthread_t thread;
+        pthread_t doctorThread;
+        pthread_t nurseThread;
+        pthread_t patientThread;
+        // if (rc = pthread_create(&thread, NULL, run, (void *)i))
+        // {
+        //     printf("Error: pthread_create() failed\n");
+        //     exit(EXIT_FAILURE);
+        // }
+        if (rc = pthread_create(&patientThread, NULL, patient, (void *) (i+1)))
+        {
+            printf("Error: pthread_create() failed\n");
+            exit(EXIT_FAILURE);
+        }
+        sleep(TIMER);
+        if (rc = pthread_create(&nurseThread, NULL, nurse, (void *) (i+1)))
+        {
+            printf("Error: pthread_create() failed\n");
+            exit(EXIT_FAILURE);
+        }
+        sleep(TIMER);
+        if (rc = pthread_create(&doctorThread, NULL, doctor, (void *) (i+1)))
+        {
+            printf("Error: pthread_create() failed\n");
+            exit(EXIT_FAILURE);
+        }
+        sleep(TIMER);
+    
+        doctors.push_back(doctorThread);
+        nurses.push_back(nurseThread);
+        patients.push_back(patientThread);
+    }
+
+    for (int i = 0; i < doctors.size(); i++)
+    {
+        pthread_join(doctors.at(i), NULL);
+        pthread_join(nurses.at(i), NULL);
+        pthread_join(patients.at(i), NULL);
+    }
+
     sem_destroy(&demoSem);
+    sem_destroy(&doctorSem);
+    sem_destroy(&nurseSem);
+    sem_destroy(&patientSem);
+    sem_destroy(&receptionistSem);
     printf("thread creation complete...\n");
     /*********************************/
-    
+
     return 0;
 }
